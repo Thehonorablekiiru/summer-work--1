710c4febf0b769b60edf30eafda2de17db3a910b
Thehonorablekiiru <joseph.kuria@techhigh.us>
2015-08-30 10:15:09 -0400

    scanner
    i learned how to get user input

--- src/summer3.java
+++ src/summer3.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
 
 public class summer3 {
-
+	public static void main(String args[]){
+        Scanner joe = new Scanner(System.in);
+        System.out.println(joe.nextLine());
+    }
 }
+
