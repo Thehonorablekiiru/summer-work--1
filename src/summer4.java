54dba0333462a887a4cd631fbec963296d02f31b
Thehonorablekiiru <joseph.kuria@techhigh.us>
2015-08-30 10:10:58 -0400

    4

---
+++ src/summer4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class summer4 {
+	public static void main(String args[]){
+        Scanner joe = new Scanner(System.in);
+        double fnum, snum, answer;
+        System.out.println("wats da 1st numba ");
+        fnum = joe.nextDouble();
+        System.out.println("wats da 2nd numba ");
+        snum = joe.nextDouble();
+        answer = fnum + snum;
+        System.out.println(answer);
+    }
+}
+
+
