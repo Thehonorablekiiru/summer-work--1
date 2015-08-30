e7f79324fc5169b57558b97dead7cbad240880e6
Thehonorablekiiru <joseph.kuria@techhigh.us>
2015-08-30 04:56:16 -0400

    m

---
+++ src/summer2.java
@@ -0,0 +1,12 @@
+
+public class summer2 {
+	  public static void main(String args[]){
+	        double hat;
+	        hat = 5.28;
+	        
+	        System.out.print("It's ");
+	        System.out.print(hat);
+	        System.out.println(" for a hat ");
+	        System.out.print("please");
+	    }
+	}
