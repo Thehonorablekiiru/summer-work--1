21d0182cfcddd4a70e427b6fd5acbc40abd2d9dc
Thehonorablekiiru <joseph.kuria@techhigh.us>
2015-08-30 10:51:02 -0400

    Increment Operators
    this was very easy

---
+++ src/summer6.java
@@ -0,0 +1,14 @@
+
+public class summer6 {
+	 public static void main(String args[]){
+		 
+	        int mango = 5;
+	        int banana = 18;
+	        mango *= 18 ;
+	        banana *= 5 ;
+	        System.out.println(mango+" mangos");
+	        System.out.println(banana+" banana");        
+
+	    }
+	}
+
