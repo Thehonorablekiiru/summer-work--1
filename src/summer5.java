c85f9e302ea9f15c1bb396dba2edf4849828c709
Thehonorablekiiru <joseph.kuria@techhigh.us>
2015-08-30 10:37:51 -0400

    math operators
    took me a bit time to work out the bugs but it works !

---
+++ src/summer5.java
@@ -0,0 +1,10 @@
+
+public class summer5 {
+	 public static void main(String args[]){
+		 int earned , hours , wage;
+		 earned =1000 ;
+		 hours = 10 ;
+		 wage = earned / hours ;
+		 System.out.println("Your wage is "+ wage);
+	 }
+}
