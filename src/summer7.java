7b30867b0009b2aede32d83698a6793205cdbaad
Thehonorablekiiru <joseph.kuria@techhigh.us>
2015-09-01 20:30:54 -0400

    If statements
    took al lot of error checking but i prevailed

---
+++ src/summer7.java
@@ -0,0 +1,13 @@
+public class summer7 {
+	 public static void main(String args[]){
+	int problems = 0;{
+
+if (problems == 0){
+System.out.println("carefree life!");
+}
+ else{
+     System.out.println("You got some problems to fix!");    
+ }
+}
+}
+}
